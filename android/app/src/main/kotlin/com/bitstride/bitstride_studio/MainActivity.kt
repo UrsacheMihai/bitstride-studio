355177c414be5e0468b041a033668fa2433e45f2
UrsacheMihai <0ursachemihaiandrei0@gmail.com>
2026-04-17 04:31:42 +0300

    Fixed the miscommit

--- android/app/src/main/kotlin/com/bitstride/bitstride_studio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.bitstride.bitstride_studio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/screens/web_export_stub.dart
+++
@@ -1,4 +0,0 @@
-﻿void downloadWebZip(List<int> bytes, String filename) {
-  throw UnsupportedError('Download zip is only supported on web for now.');
-}
-
